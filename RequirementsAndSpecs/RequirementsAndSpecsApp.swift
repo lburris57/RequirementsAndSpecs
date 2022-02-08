9ad1b019bf9224b9f2a62d2a0141210d3b592efa
Larry Burris <lburris@mac.com>
2022-02-08 13:36:00 -0500

    Initial Commit

---
+++ RequirementsAndSpecs/RequirementsAndSpecsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RequirementsAndSpecsApp.swift
+//  RequirementsAndSpecs
+//
+//  Created by Larry Burris on 2/8/22.
+//
+
+import SwiftUI
+
+@main
+struct RequirementsAndSpecsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
